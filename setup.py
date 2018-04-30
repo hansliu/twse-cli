70ea7d3d07e2785b3771e3dd6f9ca23564270f31
hcliu <hcliu@yahoo-inc.com>
2018-04-30 17:12:45 +0800

    initial

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'twsecli',
+  packages = ['twsecli'],
+  version = '0.1',
+  description = 'TWSE unofficial command-line interface',
+  author = 'Hans Liu',
+  author_email = 'hansliu.tw@gmail.com',
+  url = 'https://github.com/hansliu/twsecli',
+  download_url = 'https://github.com/hansliu/twsecli/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['twse'],
+  classifiers = [],
+)
