e5385836da094e7c6b92561439aaa5e5ec468952
hcliu <hcliu@yahoo-inc.com>
2018-05-03 00:07:36 +0800

    adjust long desc

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from distutils.core import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
   name='twsecli',
   packages=['twsecli'],
-  version='0.1.5',
+  version='0.1.6',
   description='TWSE unofficial command-line interface',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
   author='Hans Liu',
   author_email='hansliu.tw@gmail.com',
   url='https://github.com/hansliu/twsecli',
